c1c485e74c5978ce2868c778a09f2e8004aede3a
Achal Kamboj <82465596+achalkamboj@users.noreply.github.com>
2021-08-25 01:27:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "😊": "smiling",
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": "Face with Tears of Joy",
+  "😅": "Grinning Face with Sweat",
+  "😸": "Grinning Cat with Smiling Eyes",
+  "😹": "Cat with Tears of Joy"
+};
+
+var emojisWeKNow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHAndler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "we don't have this in our database";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1>Emotion Checker!</h1>
+      <input onChange={emojiInputHAndler} />
+      <h2> {meaning} </h2>
+
+      <h3> Emojis we Know </h3>
+
+      {emojisWeKNow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
